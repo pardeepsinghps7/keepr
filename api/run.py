ae42e0f06121cbea76ea4476564728d4a588b257
jatin-trigma <jatin02@trigma.in>
2025-05-15 15:03:30 +0530

    env file

--- api/run.py
+++ api/run.py
@@ -4,7 +4,7 @@ import os
 app = create_app()
 
 if __name__ == "__main__":
-    # app.run(debug=True)
 
+    # app.run(debug=True)
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
  
