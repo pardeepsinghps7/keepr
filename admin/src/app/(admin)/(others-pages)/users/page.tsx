663be8f67c01f1717522f52554b3117db722099e
Amit <amit.mittal@trigma.com>
2025-05-30 15:38:14 +0530

    changes

--- admin/src/app/(admin)/(others-pages)/users/page.tsx
+++ admin/src/app/(admin)/(others-pages)/users/page.tsx
@@ -44,7 +44,7 @@ const userColumns = [
             className="inline-flex items-center gap-2 rounded-lg bg-white px-3 py-1.5 text-sm font-medium text-gray-700 shadow-theme-xs ring-1 ring-inset ring-gray-300 transition hover:bg-gray-50 dark:bg-gray-800 dark:text-gray-400 dark:ring-gray-700 dark:hover:bg-white/[0.03]"
             title='View Items'
             >
-                Items
+                View
             </Link>
         ),
     }
--- admin/src/app/(full-width-pages)/(auth)/layout.tsx
+++ admin/src/app/(full-width-pages)/(auth)/layout.tsx
@@ -40,9 +40,9 @@ export default function AuthLayout({
               </div>
             </div>
           </div>
-          <div className="fixed bottom-6 right-6 z-50 hidden sm:block">
+          {/*<div className="fixed bottom-6 right-6 z-50 hidden sm:block">
             <ThemeTogglerTwo />
-          </div>
+          </div>*/}
         </div>
       </ThemeProvider>
     </div>
