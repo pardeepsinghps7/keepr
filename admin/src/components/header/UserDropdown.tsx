f7411c6d92ff8a3fdacbcaa8deedce90a7c2bc39
Amit <amit.mittal@trigma.com>
2025-05-30 10:56:28 +0530

    change password

--- admin/src/components/header/UserDropdown.tsx
+++ admin/src/components/header/UserDropdown.tsx
@@ -8,6 +8,7 @@ import { useRouter } from "next/navigation";
 import {EyeCloseIcon, EyeIcon} from "@/icons";
 import Label from "@/components/form/Label";
 import Input from "@/components/form/input/InputField";
+import {toast} from "react-hot-toast";
 
 export default function UserDropdown() {
   const [showPassword, setShowPassword] = useState(false);
@@ -18,7 +19,6 @@ export default function UserDropdown() {
   const [newPassword, setNewPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState("");
-  const [success, setSuccess] = useState("");
 
   const router = useRouter();
   useEffect(() => {
@@ -38,7 +38,19 @@ export default function UserDropdown() {
 
   const handleChangePassword = async () => {
     setError("");
-    setSuccess("");
+
+    if(newPassword.trim()=='' && confirmPassword.trim()=='') {
+      setError("Please enter new password and confirm password.");
+      return;
+    }
+    if(newPassword.trim()=='') {
+      setError("Please enter a new password.");
+      return;
+    }
+    if(confirmPassword.trim()=='') {
+      setError("Please enter a confirm password.");
+      return;
+    }
 
     if (newPassword !== confirmPassword) {
       setError("Passwords do not match.");
@@ -50,10 +62,12 @@ export default function UserDropdown() {
     if (error) {
       setError(error.message);
     } else {
-      setSuccess("Password updated successfully.");
+      toast.success("Password updated successfully.");
       setNewPassword("");
       setConfirmPassword("");
       setShowPasswordModal(false);
+      setShowPassword(false);
+      setShowCPassword(false);
     }
   };
 
@@ -186,7 +200,8 @@ export default function UserDropdown() {
             <div
                 className="modal-close-btn fixed inset-0 h-full w-full bg-gray-400/50 backdrop-blur-[32px]"></div>
             <div className="relative w-full max-w-[600px] rounded-3xl bg-white p-6 dark:bg-gray-900 lg:p-10">
-              <button onClick={() => setShowPasswordModal(false)} className="absolute right-3 top-3 z-999 flex h-9.5 w-9.5 items-center justify-center rounded-full bg-gray-100 text-gray-400 transition-colors hover:bg-gray-200 hover:text-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white sm:right-6 sm:top-6 sm:h-11 sm:w-11">
+              <button onClick={() => { setShowPasswordModal(false); setNewPassword("");
+                setConfirmPassword(""); }} className="absolute right-3 top-3 z-999 flex h-9.5 w-9.5 items-center justify-center rounded-full bg-gray-100 text-gray-400 transition-colors hover:bg-gray-200 hover:text-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white sm:right-6 sm:top-6 sm:h-11 sm:w-11">
                 <svg className="fill-current" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path fillRule="evenodd" clipRule="evenodd" d="M6.04289 16.5413C5.65237 16.9318 5.65237 17.565 6.04289 17.9555C6.43342 18.346 7.06658 18.346 7.45711 17.9555L11.9987 13.4139L16.5408 17.956C16.9313 18.3466 17.5645 18.3466 17.955 17.956C18.3455 17.5655 18.3455 16.9323 17.955 16.5418L13.4129 11.9997L17.955 7.4576C18.3455 7.06707 18.3455 6.43391 17.955 6.04338C17.5645 5.65286 16.9313 5.65286 16.5408 6.04338L11.9987 10.5855L7.45711 6.0439C7.06658 5.65338 6.43342 5.65338 6.04289 6.0439C5.65237 6.43442 5.65237 7.06759 6.04289 7.45811L10.5845 11.9997L6.04289 16.5413Z" fill=""></path>
                 </svg>
@@ -242,10 +257,10 @@ export default function UserDropdown() {
                   </div>
                 </div>
                 {error && <p className="text-red-500 text-sm mb-2">{error}</p>}
-                {success && <p className="text-green-500 text-sm mb-2">{success}</p>}
                 <div className="flex justify-end gap-2">
                   <button
-                      onClick={() => setShowPasswordModal(false)}
+                      onClick={() => { setShowPasswordModal(false); setNewPassword("");
+                        setConfirmPassword(""); }}
                       className="px-4 py-2 text-sm text-gray-700 dark:text-white border rounded hover:bg-gray-100 dark:hover:bg-gray-600"
                   >
                     Cancel
