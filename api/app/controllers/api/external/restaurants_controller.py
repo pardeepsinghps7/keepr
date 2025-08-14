1c9b0ca970a52c0d27238ac01e10dda3a4d67de9
Amit <amit.mittal@trigma.com>
2025-08-14 11:50:30 +0530

    restaurant api changes

--- api/app/controllers/api/external/restaurants_controller.py
+++ api/app/controllers/api/external/restaurants_controller.py
@@ -6,8 +6,8 @@ def get_all_restaurants():
 
     name = request.args.get("name", "").strip()
     near = request.args.get("near", "").strip()
-#     latitude = request.args.get("latitude", "").strip()
-#     longitude = request.args.get("longitude", "").strip()
+    latitude = request.args.get("latitude", "").strip()
+    longitude = request.args.get("longitude", "").strip()
 
     if not name:
         return jsonify({"status": False, "error": "Name is required"}), 400
@@ -19,11 +19,11 @@ def get_all_restaurants():
         "categories": "4d4b7105d754a06374d81259",
         "limit": 50,
     }
-#     if latitude and longitude:
-#         params["ll"] = f"{latitude},{longitude}"
-#         params["radius"] = 100000
-
-    params["near"] = near
+    if latitude and longitude:
+        params["ll"] = f"{latitude},{longitude}"
+        params["radius"] = 100000
+    elif near:
+        params["near"] = near
 
     FOUR_SQUARE_API_KEY = os.environ.get("FOUR_SQUARE_API_KEY")
     if not FOUR_SQUARE_API_KEY:
